9646eaa0b1dcd6735ce94057c552eeb309ca40f6
Rakibul-Hossain <rakibul.hossain@appscode.com>
2022-12-22 13:12:31 +0600

    Add support for custom admin
    Signed-off-by: Rakibul-Hossain <rakibul.hossain@appscode.com>

--- pgbouncer/reloader.sh
+++ pgbouncer/reloader.sh
@@ -5,14 +5,15 @@ echo "Reloading PgBouncer..."
 
 SSLMODE=$(grep -i "client_tls_sslmode" /etc/config/pgbouncer.ini | cut -c 22-)
 AUTHTYPE=$(grep -i "auth_type" /etc/config/pgbouncer.ini | cut -c 13-)
-PGPASSWORD=$(cat /var/run/pgbouncer/secret/pb-password)
+PASSWORD=$(cat /var/run/pgbouncer/secret/password)
+USERNAME=$(cat /var/run/pgbouncer/secret/username)
 
 if [[ "$SSLMODE" == "verify-full" ]] || [[ "$SSLMODE" == "verify-ca" ]] || [[ "$AUTHTYPE" == "cert" ]]; then
-    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=kubedb password=$PGPASSWORD sslmode=$SSLMODE sslrootcert=/var/run/pgbouncer/tls/serving/client/ca.crt sslcert=/var/run/pgbouncer/tls/serving/client/tls.crt sslkey=/var/run/pgbouncer/tls/serving/client/tls.key dbname=pgbouncer" -c RELOAD
+    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=$USERNAME password=$PASSWORD sslmode=$SSLMODE sslrootcert=/var/run/pgbouncer/tls/serving/client/ca.crt sslcert=/var/run/pgbouncer/tls/serving/client/tls.crt sslkey=/var/run/pgbouncer/tls/serving/client/tls.key dbname=pgbouncer" -c RELOAD
 elif [[ "$SSLMODE" == "require" ]]; then
-    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=kubedb password=$PGPASSWORD sslmode=$SSLMODE sslrootcert=/var/run/pgbouncer/tls/serving/client/ca.crt dbname=pgbouncer" -c RELOAD
+    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=$USERNAME password=$PASSWORD sslmode=$SSLMODE sslrootcert=/var/run/pgbouncer/tls/serving/client/ca.crt dbname=pgbouncer" -c RELOAD
 else
-    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=kubedb password=$PGPASSWORD dbname=pgbouncer" -c RELOAD
+    psql "host=localhost port=$PGBOUNCER_LISTEN_PORT user=$USERNAME password=$PASSWORD dbname=pgbouncer" -c RELOAD
 fi
 # update to reloaded status
 echo RELOADED >/etc/service/pgbouncer/pb_status
